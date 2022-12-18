6dd6abc51a09f4c8f552370aa735166b609e0e74
Ben Neal <bneal16@gmail.com>
2022-12-17 19:00:29 -0500

    populate user and fantasy team tables

--- src/model/schema/tbl_ball_team.sql
+++ src/model/schema/tbl_ball_team.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `fantasy_mgr`.`tbl_ball_team` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
+  `name_short` VARCHAR(255) NOT NULL,
   `region` VARCHAR(45) NULL,
   `seed` INT NULL,
   PRIMARY KEY (`id`),
--- src/transformations/populate_model.py
+++ src/transformations/populate_model.py
@@ -28,9 +28,9 @@ def tbl_ball_team(engine):
 
     insert_df = pandas.DataFrame(columns=['name', 'region', 'seed'])
 
-    for ball_team_name, value_dict in config.items():
+    for name, value_dict in config.items():
         data_dict = {
-            'name': [ball_team_name],
+            'name': [name],
             'region': [value_dict.get('region')],
             'seed': [value_dict.get('seed')],
         }
@@ -63,11 +63,68 @@ def tbl_player(engine):
             connection.execute(insert_query)
 
 
+def tbl_user(engine):
+    table_name = 'tbl_user'
+    with open('src/config/users.json', 'r') as f:
+        config = json.load(f)
+
+    insert_df = pandas.DataFrame(columns=['name', 'email', 'is_active'])
+
+    for name, value_dict in config.items():
+        data_dict = {
+            'name': [name],
+            'email': [value_dict.get('email')],
+            'is_active': [value_dict.get('is_active')],
+        }
+        df = pandas.DataFrame(data=data_dict)
+        # print(df)
+        insert_df = pandas.concat([insert_df, df])
+
+    print(insert_df)
+    truncate_table(engine, table_name)
+
+    logger.info(f'inserting {len(insert_df)} rows into table {table_name}')
+    insert_df.to_sql(table_name, con=e, if_exists='append', index=False)
+
+
+def tbl_fantasy_team(engine):
+    table_name = 'tbl_fantasy_team'
+    with open('src/config/fantasy_teams.json', 'r') as f:
+        config = json.load(f)
+
+    user_df = pandas.read_sql_query(f"select id, name from tbl_user", con=e)
+
+    insert_df = pandas.DataFrame(columns=['name', 'display_name', 'draft_order'])
+
+    for name, value_dict in config.items():
+        owner_user_id = user_df[user_df['name'] == value_dict.get('owner')].loc[0]['id']
+        if owner_user_id:
+            data_dict = {
+                'name': [name],
+                'display_name': [value_dict.get('display_name')],
+                'draft_order': [value_dict.get('draft_order')],
+                'fk_user_id': [owner_user_id],
+            }
+            df = pandas.DataFrame(data=data_dict)
+            # print(df)
+            insert_df = pandas.concat([insert_df, df])
+        else:
+            logger.error(f"Failed to find user owner for team {name}")
+
+    print(insert_df)
+    truncate_table(engine, table_name)
+
+    logger.info(f'inserting {len(insert_df)} rows into table {table_name}')
+    insert_df.to_sql(table_name, con=e, if_exists='append', index=False)
+
+
 def populate_model(engine, table_name):
     table_name(engine=engine)
 
 
 if __name__ == '__main__':
     e = db_util.get_engine(DATABASE_NAME)
-    populate_model(e, tbl_ball_team)
-    populate_model(e, tbl_player)
+    #populate_model(e, tbl_ball_team)
+    #populate_model(e, tbl_player)
+    #populate_model(e, tbl_user)
+    populate_model(e, tbl_fantasy_team)
