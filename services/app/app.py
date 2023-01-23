53a8af84caa59dc9ef0fb94dbfe5cb0136803cf7
ben <benvneal@gmail.com>
2023-01-22 21:58:21 -0500

    fix build errors

--- services/app/app.py
+++ services/app/app.py
@@ -54,10 +54,24 @@ def about():
 
 
 
-def create_app():
-    db_util.init_database(APP_DATABASE_NAME)
-    #app.run(host='0.0.0.0', debug=True)
-    app.run()
+# def create_app():
+#
+#     #db_util.init_database(APP_DATABASE_NAME)
+#     #app.run(host='0.0.0.0', debug=True)
+#     #app.run()
+
+def app(environ, start_response):
+    """Simplest possible application object"""
+    data = b'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
+
+
 
 #
 # if __name__ == '__main__':
